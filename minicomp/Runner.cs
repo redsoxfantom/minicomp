00b8f089397610bd22781596af9c34cfd30248c0
Tom English <thomas.f.english@lmco.com>
2019-03-06 17:41:59 -0500

    Added basig argument parsing

--- minicomp/Runner.cs
+++ minicomp/Runner.cs
@@ -19,6 +19,29 @@ namespace minicomp
         public void ParseArgs(string[] args)
         {
             logger.InfoFormat("Parsing arguments {0}", String.Join(" ", args));
+            bool debug = false;
+            string computerDefinitionFile = string.Empty;
+            string memoryDefinitionFile = string.Empty;
+            for (int i = 0; i < args.Length; i++)
+            {
+                string arg = args[i];
+                if(arg == "-d")
+                {
+                    debug = true;
+                }
+                if(arg == "-computerdefinition")
+                {
+                    i++;
+                    arg = args[i];
+                    computerDefinitionFile = arg;
+                }
+                if(arg == "-memorydefinition")
+                {
+                    i++;
+                    arg = args[i];
+                }
+            }
+
         }
 
         public void Run()
