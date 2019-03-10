6c7170db230c5ccfe46000f9867eb85e8480e8a9
Tom English <thomas.f.english@lmco.com>
2019-03-10 10:42:04 -0400

    Added factory to create processors

---
+++ minicomp.processor/ProcessorFactory.cs
@@ -0,0 +1,25 @@
+﻿using minicomp.interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace minicomp.processor
+{
+    public class ProcessorFactory
+    {
+        public static IProcessor CreateProcessor(string type)
+        {
+            if(type == "Basic")
+            {
+                return new BasicProcessor();
+            }
+            else
+            {
+                Type proctype = Type.GetType(type);
+                return (IProcessor)Activator.CreateInstance(proctype);
+            }
+        }
+    }
+}
