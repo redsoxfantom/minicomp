adcad4e82b9dd821cdd384f9ffb4cbc1bd207db1
Tom English <thomas.f.english@lmco.com>
2019-03-10 10:18:06 -0400

    Added unit tests for BasicMemoryCell

---
+++ minicomp.memory.tests/BasicMemoryCellTest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using minicomp.memory;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace minicomp.memory.tests
+{
+    [TestClass]
+    public class BasicMemoryCellTest
+    {
+        [TestMethod]
+        public void TestConstructor()
+        {
+            BasicMemoryCell cell = new BasicMemoryCell(5);
+
+            Assert.AreEqual(5, cell.Location);
+            Assert.AreEqual(0, cell.Value);
+        }
+
+        [TestMethod]
+        public void TestUpdateValue()
+        {
+            BasicMemoryCell cell = new BasicMemoryCell(0);
+            bool callbackcalled = false;
+            cell.OnMemoryCellUpdated += (c, oldVal, newVal) =>
+            {
+                Assert.AreEqual(c, cell);
+                Assert.AreEqual(0, oldVal);
+                Assert.AreEqual(1, newVal);
+                callbackcalled = true;
+            };
+
+            cell.Value = 1;
+            Assert.IsTrue(callbackcalled);
+        }
+    }
+}
---
+++ minicomp.memory.tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("minicomp.memory.tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("minicomp.memory.tests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("3caf13b4-76f9-4bda-8148-7efb7cd87069")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
