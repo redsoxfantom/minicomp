2cff1f7b7db2acc7dfd9ca96df7c07e922468b9a
Tom English <thomas.f.english@lmco.com>
2019-03-10 10:38:33 -0400

    Added factory class to create memory type

---
+++ minicomp.memory/MemoryFactory.cs
@@ -0,0 +1,25 @@
+﻿using minicomp.interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace minicomp.memory
+{
+    public class MemoryFactory
+    {
+        public static IMemory CreateMemory(string type)
+        {
+            if(type == "Basic")
+            {
+                return new BasicMemory();
+            }
+            else
+            {
+                Type memType = Type.GetType(type);
+                return (IMemory)Activator.CreateInstance(memType);
+            }
+        }
+    }
+}
